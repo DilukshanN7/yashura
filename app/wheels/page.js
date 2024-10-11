f3d1ed496832de9d06625050fc85dd959e9e75c3
Dilukshan Niranjan <65407969+DilukshanN7@users.noreply.github.com>
2024-10-11 21:26:11 +0530

    Fixed link bug

--- app/wheels/page.js
+++ app/wheels/page.js
@@ -9,82 +9,82 @@ export default function BodyKits() {
         cardData={[
           {
             title: "AV1",
-            href: "/av-1",
+            href: "wheels/av-1",
             icon: "/AV1.png",
           },
           {
             title: "AV2",
-            href: "/av-2",
+            href: "wheels/av-2",
             icon: "/AV2.png",
           },
           {
             title: "AV3",
-            href: "/av-3",
+            href: "wheels/av-3",
             icon: "/AV3.png",
           },
           {
             title: "AV4",
-            href: "/av-4",
+            href: "wheels/av-4",
             icon: "/AV4.png",
           },
           {
             title: "AV5",
-            href: "/av-5",
+            href: "wheels/av-5",
             icon: "/AV5.png",
           },
           {
             title: "AV6",
-            href: "/av-6",
+            href: "wheels/av-6",
             icon: "/AV6.png",
           },
           {
             title: "AV7",
-            href: "/av-7",
+            href: "wheels/av-7",
             icon: "/AV7.png",
           },
           {
             title: "AV8",
-            href: "/av-8",
+            href: "wheels/av-8",
             icon: "/AV8.png",
           },
           {
             title: "AV9",
-            href: "/av-9",
+            href: "wheels/av-9",
             icon: "/AV9.png",
           },
           {
             title: "AV10",
-            href: "/av-10",
+            href: "wheels/av-10",
             icon: "/AV10.png",
           },
           {
             title: "AV11",
-            href: "/av-11",
+            href: "wheels/av-11",
             icon: "/AV11.png",
           },
           {
             title: "AV12",
-            href: "/av-12",
+            href: "wheels/av-12",
             icon: "/AV12.png",
           },
           {
             title: "AV13",
-            href: "/av-13",
+            href: "wheels/av-13",
             icon: "/AV13.png",
           },
           {
             title: "AV14",
-            href: "/av-14",
+            href: "wheels/av-14",
             icon: "/AV14.png",
           },
           {
             title: "AV15",
-            href: "/av-15",
+            href: "wheels/av-15",
             icon: "/AV15.png",
           },
           {
             title: "AV16",
-            href: "/av-16",
+            href: "wheels/av-16",
             icon: "/AV16.png",
           },
         ]}
